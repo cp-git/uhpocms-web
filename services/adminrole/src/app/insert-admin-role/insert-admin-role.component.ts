6646b3f640052be1040aa9fd0bc16fbfda987a19
AnmeshR <asrapol18@gmail.com>
2022-12-19 12:37:12 +0530

    project added

---
+++ services/adminrole/src/app/insert-admin-role/insert-admin-role.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Admin } from '../admin';
+import { AdminroleServiceService } from '../adminrole-service.service';
+
+@Component({
+  selector: 'app-insert-admin-role',
+  templateUrl: './insert-admin-role.component.html',
+  styleUrls: ['./insert-admin-role.component.css']
+})
+export class InsertAdminRoleComponent {
+
+  admin=new Admin();
+  constructor(private _service:AdminroleServiceService,private _route:Router){}
+
+  addrole(){
+    this._service.addAdminRole(this.admin).subscribe(
+      data=>{
+        console.log("student added Successfully");
+        this._route.navigate([''])
+
+      },
+      erorr=>console.log("exception")
+    )
+  
+  }
+
+  goBack(){
+    this._route.navigate([''])
+
+  }
+}
