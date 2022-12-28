4f3e08bc4a78834504cbf0f4796d676be76f3445
mayur238 <mayur.p@cloudpointamerica.com>
2022-12-28 15:30:29 +0530

    changed in update

--- services/src/app/admindepartment/department/department.component.ts
+++ services/src/app/admindepartment/department/department.component.ts
@@ -66,15 +66,21 @@ export class DepartmentComponent implements OnInit {
   }
 
   updateDepartment(dept: Department) {
+    this.department = ({} as Department);
+    this.department.id = dept.id;
+    this.department.name = dept.name;
+    this.department.description = dept.description;
+    this.department.active = dept.active;
+    this.department.institutionId_id = dept.institutionId_id;
 
-    this._deptService.updateDepartment(dept.name, dept).subscribe(
+    this._deptService.updateDepartment(dept.name, this.department).subscribe(
       response => {
         this.department = response;
         this.departments[this.departments.indexOf(dept)] = this.department;
         alert("Updated Successfuly");
       },
       error => {
-        alert("not able to add data \n" + JSON.stringify(error.error));
+        alert("not able to update data \n" + JSON.stringify(error.error));
       }
     );
   }
