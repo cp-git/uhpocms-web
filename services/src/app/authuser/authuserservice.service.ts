8c08781ec4de89d837048faa6a467800529f87db
Shradha2105 <shradha.p@cloudpointamerica.com>
2023-01-04 11:32:04 +0530

    merging changes

--- services/src/app/authuser/authuserservice.service.ts
+++ services/src/app/authuser/authuserservice.service.ts
@@ -11,23 +11,23 @@ export class AuthuserserviceService {
   constructor(private _http:HttpClient) { }
   
   authUserList():Observable<any>{
-    return this._http.get<any>("http://localhost:8090/uhpocms/authuser?username=all");
+    return this._http.get<any>("http://localhost:8090/authuser/uhpocms/authuser?username=all");
   }
 
   addAuthUser(authuser:Authuser): Observable<any> {
-    return this._http.post<any>("http://localhost:8090/uhpocms/authuser",authuser);
+    return this._http.post<any>("http://localhost:8090/authuser/uhpocms/authuser",authuser);
   }
 
  deleteAuthUser(authUserName: string): Observable<any> {
-    return this._http.delete<any>("http://localhost:8090/uhpocms/authuser/"+authUserName);
+    return this._http.delete<any>("http://localhost:8090/authuser/uhpocms/authuser/"+authUserName);
   }
 
   getAuthUser(authUserName:string): Observable<any> {
-    return this._http.get<any>("http://localhost:8090/uhpocms/authuser/"+authUserName);
+    return this._http.get<any>("http://localhost:8090/authuser/uhpocms/authuser/"+authUserName);
   }
 
   updateAuthUser(authUserName:string,authuser:Authuser): Observable<any> {
     
-    return this._http.put<any>("http://localhost:8090/uhpocms/authuser/"+authUserName,authuser);
+    return this._http.put<any>("http://localhost:8090/authuser/uhpocms/authuser/"+authUserName,authuser);
   }
 }
