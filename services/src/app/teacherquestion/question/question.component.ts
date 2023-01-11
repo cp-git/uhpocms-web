f123c686f64b92e0ad4945e1f4bcbf1d3da4aa1a
kaushik6565 <kaushik.w@cloudpointamerica.com>
2023-01-11 18:03:03 +0530

    put operation

--- services/src/app/teacherquestion/question/question.component.ts
+++ services/src/app/teacherquestion/question/question.component.ts
@@ -9,15 +9,15 @@ import { QuestionService } from '../question.service';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent implements OnInit {
-  question = new Question ();
-  _question : Question [] = [];
+  question = new Question();
+  _question: Question[] = [];
   constructor(private _service: QuestionService, private _activeRoute: ActivatedRoute, private _route: Router) { }
-  
 
-  insertQuestion(que :Question){
+
+  insertQuestion(que: Question) {
     alert(JSON.stringify(que));
     this.question.questionFigure = que.questionFigure;
-    this.question.questionContent =que.questionContent;
+    this.question.questionContent = que.questionContent;
     this.question.questionExplanation = que.questionExplanation;
     this.question.questionOrderNo = que.questionOrderNo;
     this.question.questionIsMCQ = que.questionIsMCQ;
@@ -26,29 +26,66 @@ export class QuestionComponent implements OnInit {
     this.question.questionIsActive = que.questionIsActive;
     this._service.addQuestion(this.question).subscribe(
 
-      
 
-      data =>{
+
+      data => {
         alert(JSON.stringify(que));
         alert("Question added Successfully")
         this.ngOnInit();
       },
-      error=>
+      error =>
         alert("please enter valid details")
-      
+
     )
 
 
   }
 
+  updateQuestion(que: Question) {
+
+    this.question.questionId = que.questionId;
+    this.question.questionFigure = que.questionFigure;
+    this.question.questionContent = que.questionContent;
+    this.question.questionExplanation = que.questionExplanation;
+    this.question.questionOrderNo = que.questionOrderNo;
+    this.question.questionIsMCQ = que.questionIsMCQ;
+    this.question.questionQuizId = que.questionQuizId;
+    this.question.questionCategoryId = que.questionCategoryId;
+    this.question.questionIsActive = que.questionIsActive;
+    this._service.updatedQuestion(this.question.questionFigure, this.question).subscribe(
+      data => {
+
+        alert("here" + JSON.stringify(data))
+        alert("updated successfully")
+        this.ngOnInit();
+      },
+      error => alert("please enter valid details")
+
+    )
+  }
+
+  deleteQuestion(que: Question) {
+    this._service.deleteQuestion(que.questionFigure).subscribe(
+
+      data => {
+        alert("question is deleted" + JSON.stringify(que.questionFigure))
+        this.ngOnInit();
+
+      },
+      error => alert("Failed to delete Question")
 
 
-  
-  ngOnInit() : void{
+    )
+
+  }
+
+
+
+  ngOnInit(): void {
     this._service.questionList().subscribe(
       data => {
         console.log("Response");
-        this._question =data;
+        this._question = data;
       },
       Error => console.log("exception")
     )
