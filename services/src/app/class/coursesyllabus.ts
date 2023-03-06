cc515f441ddcd361eeee54bd4b53b2a4af42b3f7
kaushik6565 <kaushik.w@cloudpointamerica.com>
2023-03-06 12:45:44 +0530

    final

---
+++ services/src/app/class/coursesyllabus.ts
@@ -0,0 +1,10 @@
+export class Coursesyllabus {
+    courseSyllabusId!: number;
+    syllabusFile!: string;
+    courseId!: number;
+    courseSyllabusIsActive!: boolean;
+    createdBy!: string;
+    createdOn!: Date;
+    modifiedBy!: String;
+    modifiedOn!: Date;
+}
