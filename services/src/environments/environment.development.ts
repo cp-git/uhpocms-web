aab21e93c008549528a138f1752e59cc1551051c
mayur238 <mayur.p@cloudpointamerica.com>
2023-04-12 10:24:17 +0530

    change in ports

--- services/src/environments/environment.development.ts
+++ services/src/environments/environment.development.ts
@@ -37,7 +37,7 @@ export const environment = {
   syllabusUrl: `http://localhost:8090/courseSyllabus/uhpocms/courseSyllabus`,
   assignTeacherUrl: 'http://localhost:8090/assigntoteacher/uhpocms/assigntoteacher',
   accesscontrolUrl: 'http://localhost:8090/accesscontrol/uhpocms/access',
-  answerUrl: 'http://localhost:8080/uhpocms'
+  answerUrl: 'http://localhost:8090/answer/uhpocms'
 
 
 };
