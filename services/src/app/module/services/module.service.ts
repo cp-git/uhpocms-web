b604aba1636728ea8386269703a0a32e61dc35ec
Akashd1211 <akash.d@cloudpointamerica.com>
2023-04-13 10:21:33 +0530

    After testing Fixing bug

--- services/src/app/module/services/module.service.ts
+++ services/src/app/module/services/module.service.ts
@@ -69,6 +69,6 @@ export class ModuleService {
   }
 
   getModuleByCourseId(moduleId: number): Observable<Module[]> {
-    return this._http.get<Module[]>(`${this.moduleUrl}/module/courseId/${moduleId}`);
+    return this._http.get<Module[]>(`${this.moduleUrl}/courseId/${moduleId}`);
   }
 }
