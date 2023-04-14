4018c44e48edaf9f5b38f7f4060004dd839fd3ab
kaushik6565 <kaushik.w@cloudpointamerica.com>
2023-04-14 13:40:38 +0530

    enroll to student ui

--- services/src/app/question/components/question-answer/question-answer.component.ts
+++ services/src/app/question/components/question-answer/question-answer.component.ts
@@ -14,12 +14,66 @@ export class QuestionAnswerComponent {
   @Input() disable: boolean = false;
 
   @Input() selectedCategoryId: any;
+  @Input() selectedCategoryName: any;
   @Input() selectedQuizId: any;
 
+  // @Input() mcqAnswer: any;
+  //  @Input() options :any;
   @Output() submitClicked: EventEmitter<any> = new EventEmitter();
+
+
+  mcqAnswer: any;
+  options = ['Option 1', 'Option 2', 'Option3', 'Option4'];
+
 
   // for pagination 
   currentPage = 1;
+  page = 1;
+  selectedCategory: string;
+  isMcqFormVisible: boolean = false;
+  isNonMcqFormVisible: boolean = false;
+
+
+
+  addOption() {
+    this.options.push('Option ' + (this.options.length + 1));
+  }
+  // Define a function to handle page changes
+
+  removeOption(index: any) {
+    this.options.splice(index, 1);
+  }
+
+
+  onCategorySelected() {
+    if (this.selectedCategory === 'mcq') {
+      this.isMcqFormVisible = true;
+      this.isNonMcqFormVisible = false;
+    } else if (this.selectedCategory === 'non-mcq') {
+      this.isMcqFormVisible = false;
+      this.isNonMcqFormVisible = true;
+    } else {
+      this.isMcqFormVisible = false;
+      this.isNonMcqFormVisible = false;
+    }
+  }
+
+  //pagination
+
+  sessionData: any;
+  data: any;
+
+  imageUrl: string | undefined;
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageUrl = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
 
 
   ngOnChanges(changes: SimpleChanges): void {
