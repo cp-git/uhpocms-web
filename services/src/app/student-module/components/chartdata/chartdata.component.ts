6dc1acb864f25d60c31f68c252ae5d306fe0ea89
Shradha2105 <shradha.p@cloudpointamerica.com>
2023-05-11 12:37:28 +0530

    donut chart for simaulated dat

--- services/src/app/student-module/components/chartdata/chartdata.component.ts
+++ services/src/app/student-module/components/chartdata/chartdata.component.ts
@@ -2,11 +2,15 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Chart ,registerables} from 'chart.js';
 import { Coursesyllabus } from 'app/class/coursesyllabus';
 import { CourseProgressService } from 'app/courseProgress/services/course-progress.service';
+
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+Chart.register(ChartDataLabels);
 Chart.register(...registerables);
 @Component({
   selector: 'app-chartdata',
   templateUrl: './chartdata.component.html',
-  // template: '<canvas id="myChart"></canvas>',
+  
   styleUrls: ['./chartdata.component.css']
 })
 export class ChartdataComponent {
@@ -15,104 +19,25 @@ export class ChartdataComponent {
 
   }
 
-  // ngOnInit() {
-
-    // this.courProgServ.getAllCourseProgress().subscribe(data => {
-    //   const formattedData = data.map(item => {
-    //     return {
-    //       label: item.courseId,
-    //       value: item.progress
-    //     };
-    //   });
-
-      
-    //   const ctx = document.getElementById('pieChart') as HTMLCanvasElement;
-    //   if (ctx) {
-    //   const pieChart = new Chart(ctx, {
-    //     type: 'doughnut',
-    //     data: {
-    //       datasets: [{
-    //         data: formattedData.map(item => item.value),
-    //         backgroundColor: [
-    //           'rgba(255, 99, 132, 0.5)',
-    //           'rgba(54, 162, 235, 0.5)'
-              
-    //         ]
-    //       }],
-    //       labels: formattedData.map(item => item.label)
-    //     },
-    //     options: {
-    //       responsive: true,
-          
-    //     }
-    //   });
-    // }
-    
-  //   const data = {
-  //     labels: [
-  //       'Red',
-  //       'Blue',
-  //       'Yellow'
-  //     ],
-  //     datasets: [{
-  //       label: 'My First Dataset',
-  //       data: [300, 50, 100],
-  //       backgroundColor: [
-  //         'rgb(255, 99, 132)',
-  //         'rgb(54, 162, 235)',
-  //         'rgb(255, 205, 86)'
-  //       ],
-  //       hoverOffset: 4
-  //     }]
-  //   };
-  //   // });
-  //   const config = {
-  //     type: 'doughnut',
-  //     data: data,
-  //   };
-  // const ctx = document.getElementById('myChart')  as HTMLCanvasElement;
-
-  // new Chart(ctx, {
-  //   type: 'doughnut',
-  //   data: {
-  //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  //     datasets: [{
-  //       label: '# of Votes',
-  //       data: [12, 19, 3, 5, 2, 3],
-  //       borderWidth: 1
-  //     }]
-  //   },
-  //   options: {
-  //     scales: {
-  //       y: {
-  //         beginAtZero: true
-  //       }
-  //     }
-  //   }
-  
-  // });
+ 
 
   chart: any;
 
   @ViewChild('dChart', { static: false })
   dChart!: ElementRef;
-  @Input() jsonArray: any = [25, 15, 20, 15, 30];
+  @Input() jsonArray: any = [15,20];
   @Input() chartLabels: any = [
-    'label 1',
-    'label 2',
-    'label 3',
-    'label 4',
-    'label 5',
+    'Completed'
+    ,
+    'Remaining'
+    
   ];
   @Input() cutOut: number = 75;
   @Input() backgroundColors: any = [
-    '#E15D44',
-    '#55B4B0',
-    '#DFCFBE',
-    '#9B2335',
-    '#5B5EA6',
+    "#E15D44", "#55B4B0"
+ 
   ];
-
+  
   ngAfterViewInit() {
     let cvs: any;
     cvs = this.dChart.nativeElement;
@@ -128,34 +53,29 @@ export class ChartdataComponent {
             borderWidth: 0,
           },
         ],
+      },
+      
+      plugins: [ChartDataLabels],
+      options: {
+        plugins: {
+          datalabels: {
+            formatter: function(value, context) {
+              return Math.round(value) + '%';
+            },
+            color: 'black'
+          }
+        }
       }
-      // ,
-      // options: {
-      //   responsive: false,
-      //   legend: {
-      //     display: false,
-      //   },
-      //   cutoutPercentage: this.cutOut,
-      //   tooltips: {
-      //     enabled: true,
-      //   },
-      //   layout: {
-      //     padding: {
-      //       left: 0,
-      //       right: 0,
-      //       top: 0,
-      //       bottom: 0,
-      //     },
-      //   },
-      // },
+      
+     
     });
   }
   
 
 
-  // }
+  }
 
-}
+
 
 
   
--- services/src/app/student-panel/components/student-panel/student-panel.component.ts
+++ services/src/app/student-panel/components/student-panel/student-panel.component.ts
@@ -2,8 +2,12 @@ import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as dataOne from '../../../dataOne.json';
 import * as dataThree from '../../../dataThree.json';
-import { Chart } from 'chart.js/dist';
+
 import { ChartdataComponent } from 'app/student-module/components/chartdata/chartdata.component';
+import { CourseProgressService } from 'app/courseProgress/services/course-progress.service';
+import { CourseProgress } from 'app/courseProgress/class/courseprogress';
+import { TeacherCourseService } from 'app/teacher-course/services/teacher-course.service';
+import { Course } from 'app/teacher-course/class/course';
 
 @Component({
   selector: 'app-student-panel',
@@ -15,18 +19,54 @@ export class StudentPanelComponent {
   arrTwo: any = dataOne.data[0];
   arrThree: any = dataThree.data[0];
 
+  course:Course= new Course();
   @ViewChild(ChartdataComponent) dChart: any;
 
-  doughnutCharts: any = [this.arrOne, this.arrTwo, this.arrThree];
+  
+  doughCharts:any=[];
+  criteriaVar:number=0;
+  numOfChartsDisplayed:number=0;
+  currentIndex: number = 0;
+  doughnutCharts: { category: string, numbers: number[], labels: string[], colors: string[] }[] = 
 
+  [
+    {
+        "category": "Advertising",//course id //dynamic
+        "numbers": [5,15,30,35,15],
+        "labels": ["Google", "Facebook", "Instagram", "Amazon", "YouTube"], //Complete and Incomplte //static
+        "colors": ["#E15D44", "#55B4B0", "#DFCFBE", "#9B2335", "#5B5EA6"]  //any two colours
+    },
+//  [
+//   [
+  {
+    category: "",
+    numbers: []
+    ,
+    labels: [],
+    colors: []
+  },
+  {
+    category: "",
+    numbers: []
+    ,
+    labels: [],
+    colors: []
+  },
+];   
+
+  
+  
+  
+  
+  
 
   // Declare class properties
   profileId: any;
-
+  courseProgressArr: CourseProgress[] = [];
   userName!: string;
 
   // Inject Router and ActivatedRoute services into constructor
-  constructor(private _route: Router, private _activatedRoute: ActivatedRoute) {
+  constructor(private _route: Router, private _activatedRoute: ActivatedRoute,private courseProgServ:CourseProgressService,private courseService:TeacherCourseService) {
 
   }
 
@@ -36,7 +76,72 @@ export class StudentPanelComponent {
     this.profileId = this._activatedRoute.snapshot.paramMap.get('id');
     this.userName = this._activatedRoute.snapshot.params['userName'];
     console.log(this.profileId);
+    this.getAllCourseProgress();
+    this.loadMoreCharts();
+    
+    console.log(this.currentIndex)
+    // this.showNextButton();
   }
+
+  
+    async getAllCourseProgress(){
+      let filteredCouProgArr:CourseProgress[]=[];
+  this.courseProgServ.getAllCourseProgress().subscribe(
+    async (response)=>{
+      this.courseProgressArr = response;
+
+      filteredCouProgArr = this.courseProgressArr.filter((element)=>element.studentId == this.profileId);
+       for(let i=0; i<filteredCouProgArr.length; i++){
+        const remainingPercentage:number = 100-filteredCouProgArr[i].progress;
+        // this.doughnutCharts[i].numbers= [0, 0];
+        // this.doughnutCharts[i].numbers= [this.courseProgressArr[i].progress, remainingPercentage];
+        // this.doughnutCharts[i].category = (this.courseProgressArr[i].courseId).toString();
+
+        await this.getCourseNameById(filteredCouProgArr[i].courseId);
+
+        this.doughCharts[i] = [filteredCouProgArr[i].progress, remainingPercentage, this.course.courseName];
+      }
+
+      console.log(this.doughCharts)
+      this.criteriaVar = this.doughCharts.length - 3;
+      console.log(Array.isArray(this.doughnutCharts))
+    }
+  )
+}
+loadMoreCharts() {
+  this.numOfChartsDisplayed += 3;
+}
+next() {
+  // if (this.currentIndex < this.doughCharts.length - 4) {
+    this.currentIndex += 3;
+    this.getAllCourseProgress();
+    console.log(this.currentIndex)
+  // }
+}
+
+previous() {
+
+  this.currentIndex -= 3;
+  this.getAllCourseProgress();
+}
+
+showNextButton() {
+  return this.currentIndex < this.doughCharts.length - 3;
+}
+getCourseNameById(courseId:number){
+  return new Promise<void>((resolve, reject) => {
+    this.courseService.getCourseByCourseId(courseId).subscribe(
+      (data) => {
+        this.course = data;
+        resolve();
+      },
+      (error) => {
+        reject(error);
+      }
+    )
+  });
+}
+
 
   // Navigate to student course page with current profileId
   RedirectToStudentCourse() {
